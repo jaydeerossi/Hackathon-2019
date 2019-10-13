9c27680a7b910355ef4f8fa81aadd58101c7e840
jaydeerossi <52835439+jaydeerossi@users.noreply.github.com>
2019-10-13 11:53:04 -0400

    Update app.py

--- multipage_app/app.py
+++ multipage_app/app.py
@@ -1,7 +1,9 @@
 import dash
+import flask
 
 external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server = server)
 app.config.suppress_callback_exceptions = True
